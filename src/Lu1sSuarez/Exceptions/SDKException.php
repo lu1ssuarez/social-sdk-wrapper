22fae2cf5626530d6395c9c80a0b9974d98e431c
Luis Enrique Suárez <luissuarezdev@gmail.com>
2016-05-13 21:59:31 -0400

    list sdk current

---
+++ src/Lu1sSuarez/Exceptions/SDKException.php
@@ -0,0 +1,12 @@
+<?php
+    
+    namespace Lu1sSuarez\Exceptions;
+    
+    /**
+     * Class SDKException
+     *
+     * @package Lu1sSuarez\Exceptions
+     */
+    class SDKException extends \Exception {
+        
+    }
--- src/Lu1sSuarez/Social.php
+++ src/Lu1sSuarez/Social.php
@@ -1,7 +1,9 @@
 <?php
     
     namespace Lu1sSuarez;
-
+    
+    use Lu1sSuarez\Exceptions\SDKException;
+    
     /**
      * Class Social
      *
@@ -12,10 +14,27 @@
          * @const string Version number of the Social SDK Wrapper.
          */
         const VERSION = '1.1.0';
-    
+        
+        /**
+         * @var array List of permitted SDK
+         */
+        protected $sdk;
+        
         /**
          * Social constructor.
+         *
+         * @param string $sdk
+         * @param array  $config
+         *
+         * @throws SDKException
          */
-        public function __construct() { }
-    
+        public function __construct($sdk, array $config) {
+            $this->sdk = ['facebook'];
+            
+            if (!$this->sdk[$sdk]) {
+                throw new SDKException('The chosen sdk is currently not available, if you want to collaborate in the incorporation of ' . $sdk . ' we invite you to join our partnership. Available SDKs ' . implode(",", $this->sdk) . '.');
+            }
+            
+        }
+        
     }
