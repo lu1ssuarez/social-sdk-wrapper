f2d9132226ae51b1fda3d1289331c9223d6b0898
Luis Enrique Suárez <luissuarezdev@gmail.com>
2016-05-13 21:24:11 -0400

    class social

---
+++ src/Lu1sSuarez/Social.php
@@ -0,0 +1,21 @@
+<?php
+    
+    namespace Lu1sSuarez;
+
+    /**
+     * Class Social
+     *
+     * @package Lu1sSuarez
+     */
+    class Social {
+        /**
+         * @const string Version number of the Social SDK Wrapper.
+         */
+        const VERSION = '1.1.0';
+    
+        /**
+         * Social constructor.
+         */
+        public function __construct() { }
+    
+    }
